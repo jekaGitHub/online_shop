164abd7388793518825c1cc5b5c5936b86d42b68
Evgeny Safonov <jeka-luda@yandex.ru>
2024-02-17 20:45:38 +0300

    change category and add manufactured_at

--- catalog/models.py
+++ catalog/models.py
@@ -20,10 +20,11 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name="Наименование")
     description = models.TextField(**NULLABLE, verbose_name="Описание")
     image = models.ImageField(upload_to="photo", verbose_name="Превью", **NULLABLE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="catalog", verbose_name="Категория")
     price = models.DecimalField(decimal_places="2", max_digits="7", verbose_name="Цена за покупку")
     created_at = models.DateField(verbose_name="Дата создания")
     updated_at = models.DateField(verbose_name="Дата последнего изменения")
+    manufactured_at = models.DateField(default="2024-02-17", verbose_name="Дата производства продукта")
 
     def __str__(self):
         return f'{self.name} ({self.category})'
