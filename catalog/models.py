bea57dd1fb098558cf7de8b87b113cad132e0505
Evgeny Safonov <jeka-luda@yandex.ru>
2024-02-20 23:16:23 +0300

    change models.py and migrate

--- catalog/models.py
+++ catalog/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     description = models.TextField(**NULLABLE, verbose_name="Описание")
     image = models.ImageField(upload_to="photo", verbose_name="Превью", **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="catalog", verbose_name="Категория")
-    price = models.DecimalField(decimal_places="2", max_digits="7", verbose_name="Цена за покупку")
+    price = models.DecimalField(decimal_places=2, max_digits=7, verbose_name="Цена за покупку")
     created_at = models.DateField(verbose_name="Дата создания")
     updated_at = models.DateField(verbose_name="Дата последнего изменения")
     manufactured_at = models.DateField(default="2024-02-17", verbose_name="Дата производства продукта")
