06b10f811809c61299f9bbb928fdb2074d934332
Evgeny Safonov <jeka-luda@yandex.ru>
2024-03-03 22:27:30 +0300

    create model Article

--- catalog/models.py
+++ catalog/models.py
@@ -31,3 +31,20 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Название")
+    slug = models.CharField(max_length=150, verbose_name="slug", **NULLABLE)
+    description = models.TextField(verbose_name="Содержимое")
+    image = models.ImageField(upload_to="photo/article", verbose_name="Превью", **NULLABLE)
+    created_at = models.DateField(verbose_name="Дата создания")
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
+    views_count = models.IntegerField(default=0, verbose_name="Просмотры")
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
