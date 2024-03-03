893b235c3ef2be8250e1e0a583b727a71fb6bb4b
Evgeny Safonov <jeka-luda@yandex.ru>
2024-03-03 23:56:49 +0300

    change code

--- catalog/views.py
+++ catalog/views.py
@@ -44,10 +44,20 @@ class ArticleCreateView(CreateView):
 class ArticleListView(ListView):
     model = Article
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class ArticleDetailView(DetailView):
     model = Article
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
 
 class ArticleUpdateView(UpdateView):
     model = Article
