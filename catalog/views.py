2876b41bac51da0a80934ca9b33e4e7adafb0c52
Evgeny Safonov <jeka-luda@yandex.ru>
2024-03-04 22:50:49 +0300

    comment code

--- catalog/views.py
+++ catalog/views.py
@@ -27,13 +27,13 @@ def home(request):
     return render(request, 'catalog/index.html', сontext)
 
 
-def contact(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'Вы отправили новое сообщение от {name}({phone}): {message}')
-    return render(request, 'catalog/contact.html')
+# def contact(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         phone = request.POST.get('phone')
+#         message = request.POST.get('message')
+#         print(f'Вы отправили новое сообщение от {name}({phone}): {message}')
+#     return render(request, 'catalog/contact.html')
 
 
 class ArticleCreateView(CreateView):
