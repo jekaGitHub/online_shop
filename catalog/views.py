eb2ce4b285ced92418501fc2ddf6f4795de60537
Evgeny Safonov <jeka-luda@yandex.ru>
2024-03-17 18:44:46 +0300

    add code

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from pytils.translit import slugify
 
+from catalog.forms import ProductForm
 from catalog.models import Product, Article
 from django.views.generic import View, CreateView, ListView, DetailView, UpdateView, DeleteView, TemplateView
 
@@ -38,6 +39,18 @@ class ContactView(View):
 
     def get(self, request):
         return render(request, 'catalog/contact.html')
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:product_list')
 
 
 class ArticleCreateView(CreateView):
