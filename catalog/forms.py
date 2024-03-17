e9cb2c6cf601965be2bd1ec5f7a8f2dbce68262d
Evgeny Safonov <jeka-luda@yandex.ru>
2024-03-17 22:44:04 +0300

    add clean_methods

--- catalog/forms.py
+++ catalog/forms.py
@@ -7,3 +7,17 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+
+        if cleaned_data in ('казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар'):
+            raise forms.ValidationError('Название содержит запрещённые слова')
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+
+        if cleaned_data in ('казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар'):
+            raise forms.ValidationError('Описание содержит запрещённые слова')
+        return cleaned_data
