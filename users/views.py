062df8730ca80745b918326972f9d05edebd95fd
Evgeny Safonov <jeka-luda@yandex.ru>
2024-03-24 23:47:45 +0300

    add code

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import RegistrationForm
+from users.models import User
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = RegistrationForm
+
+    def get_success_url(self):
+        return reverse('users:login')
